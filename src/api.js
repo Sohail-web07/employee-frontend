1b042bab59ecd63c137c636d209e1c7494be3eb0
Sohail-web07 <sohail_shaikh@bpointer.in>
2025-06-16 14:59:29 +0530

    Updated base URL in api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/employees', // Use your backend URL
+  //baseURL: 'http://localhost:5000/api/employees', // Use your backend URL
+  baseURL: 'https://employee-backend-1q7b.onrender.com' 
 });
